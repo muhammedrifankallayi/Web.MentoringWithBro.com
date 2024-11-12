670c9e9319a956f73566adc95c0a4f528dc1037c
muhammed rifan <rifanmuhammed173@gmail.com>
2024-11-13 00:28:33 +0530

    form compled weekly task add

--- src/app/popups/add-week-detail-popup/add-week-detail-popup.component.ts
+++ src/app/popups/add-week-detail-popup/add-week-detail-popup.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
@@ -6,17 +7,27 @@ import { MatDialogRef } from '@angular/material/dialog';
   templateUrl: './add-week-detail-popup.component.html',
   styleUrls: ['./add-week-detail-popup.component.css']
 })
-export class AddWeekDetailPopupComponent {
+export class AddWeekDetailPopupComponent implements OnInit {
+
+itemForm!:FormGroup
 
 constructor(
-  public dialogRef:MatDialogRef<AddWeekDetailPopupComponent>
+  public dialogRef:MatDialogRef<AddWeekDetailPopupComponent>,
+  private fb:FormBuilder
 ){}
 
 
-
+ngOnInit(): void {
+  this.buildForm()
+}
 
 buildForm(){
-  
+ this.itemForm = this.fb.group({
+  week:[0],
+  taskName:[""],
+  description:[""],
+  isCompleted:[false]
+ })
 }
 
 }
